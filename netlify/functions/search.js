8f25eb3c265fd11a0ba0e6a32f6dec5d06b7a665
Hello-p772 <Harry_Starr@Icloud.com>
2026-05-15 20:12:28 +0100

    y

--- netlify/functions/search.js
+++ netlify/functions/search.js
@@ -3,31 +3,36 @@ const axios = require('axios');
 exports.handler = async (event) => {
     const query = event.queryStringParameters.q;
 
+    if (!query) {
+        return { statusCode: 400, body: "No query" };
+    }
+
     try {
-        // We use a specific Google URL that is easier to load in an iframe
-        const googleUrl = `https://www.google.com/search?q=${encodeURIComponent(query)}&gbv=1`;
-        
-        const response = await axios.get(googleUrl, {
+        // We use the 'mobile' version of Google because it's lighter and easier to proxy
+        const response = await axios.get('https://www.google.com/search', {
+            params: { q: query, gbv: "1" },
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+                'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36'
             }
         });
 
-        // This line is the "Magic": it fixes Google's internal links so they don't loop back to you
-        let formattedContent = response.data.replace(/href="\/search/g, 'href="https://www.google.com/search');
+        // This removes Google's "protection" that prevents it from being shown in frames
+        let html = response.data;
+        html = html.replace(/href="\//g, 'href="https://www.google.com/');
 
         return {
             statusCode: 200,
             headers: {
                 "Content-Type": "text/html",
-                "Access-Control-Allow-Origin": "*"
+                "Access-Control-Allow-Origin": "*",
+                "X-Frame-Options": "ALLOWALL" 
             },
-            body: formattedContent
+            body: html
         };
     } catch (error) {
         return {
             statusCode: 500,
-            body: "The search engine is sleepy. Please try again in 10 seconds."
+            body: "Error fetching results. Google might be blocking the request."
         };
     }
 };
